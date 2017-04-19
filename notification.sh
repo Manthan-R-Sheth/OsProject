f032ab4e5a614d14571c146053ffcd8deb6667ec
manthan-r-sheth <manthanrsheth96@gmail.com>
2017-04-19 22:54:04 +0530

    Add Notifications and sound

---
+++ notification.sh
@@ -0,0 +1,6 @@
+spd-say "Process Started"
+start=$(date +%s)
+"$@"
+[ $(($(date +%s) - start)) -le 15 ] || notify-send "Notification" "Long\
+running command \"$(echo $@)\" took $(($(date +%s) - start)) seconds to finish"
+spd-say "Process Completed"
